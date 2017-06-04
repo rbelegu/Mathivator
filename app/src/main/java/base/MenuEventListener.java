87145c4f4dfc28f242b4eb5ccbaac9ea5f1192b3
gzim <gzim.ramizi@students.ffhs.ch>
2017-06-04 16:02:43 +0200

    Comment

--- app/src/main/java/base/MenuEventListener.java
+++ app/src/main/java/base/MenuEventListener.java
@@ -7,19 +7,34 @@ import android.util.Log;
 import com.example.admin.mathivator.R;
 
 /**
- * Created by admin on 24.05.2017.
+ * Class/Listener "MenuEventListener" as Singleton to handle Options-Menu Events.
+ *
+ * @author N. Hafen
+ * @version 1.0
  */
 
 public class MenuEventListener {
     private static MenuEventListener INSTANCE = new MenuEventListener();
     private static final String LOG_TAG = MenuEventListener.class.getSimpleName();
 
+    /*
+    * Method to get an Instance of this Listener.
+    *
+    * @return Instance of MenuEventListener
+    * */
     public static MenuEventListener getInstance() {
         return INSTANCE;
     }
     private MenuEventListener() {
     }
 
+    /*
+    * Main method to handle all menuentries based on their IDs.
+    *
+    * @param activity Ancestor of Activities
+    * @param id       Name/ID of the clicked activity.
+    *
+    * */
     public void activateMenu(AppCompatActivity activity, int id){
         switch(id){
             case R.id.menuhome :
@@ -37,6 +52,12 @@ public class MenuEventListener {
         }
     }
 
+    /*
+    * Method to call the "Rangliste".
+    *
+    * @param activity Ancestor of Activity
+    *
+    * */
     private void goToHighscore(AppCompatActivity activity){
         try{
             Intent nextScreen = new Intent(activity, HighscoreActivity.class);
@@ -46,6 +67,12 @@ public class MenuEventListener {
         }
     }
 
+    /*
+    * Method to call the "Einstellungen".
+    *
+    * @param activity Ancestor of Activity
+    *
+    * */
     private void goToSettings(AppCompatActivity activity){
         try{
             Intent nextScreen = new Intent(activity, SettingsActivity.class);
@@ -55,6 +82,12 @@ public class MenuEventListener {
         }
     }
 
+    /*
+    * Method to call the "Start".
+    *
+    * @param activity Ancestor of Activity
+    *
+    * */
     private void goToMain(AppCompatActivity activity){
         try{
             Intent nextScreen = new Intent(activity, MainActivity.class);
@@ -63,7 +96,12 @@ public class MenuEventListener {
             Log.d(LOG_TAG,"Error in goToMain " + e );
         }
     }
-
+    /*
+    * Method to run the "Game".
+    *
+    * @param activity Ancestor of Activity
+    *
+    * */
     private void goToPlay(AppCompatActivity activity){
         try{
             Intent nextScreen = new Intent(activity, GameActivity.class);
